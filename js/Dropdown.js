8d9aa582580591a03646176ac9c3641ba63474ac
shmakovdima <contact@shmakovdima.ru>
2017-03-31 17:14:23 +0300

    init dropdown

---
+++ js/Dropdown.js
@@ -0,0 +1,7 @@
+(function (window) {
+	'use strict';
+
+	// Публикуем ссылку на конструктор
+	window.getNameSpace('ru.mail.cpf.modules').Dropdown = Dropdown;
+
+})(window);
--- js/initDropdown.js
+++ js/initDropdown.js
@@ -6,11 +6,18 @@
   var dropdownParams = {
     cssSels: {
       modificators: {
-        activeClass: 'dropdown__item__active', // класс активного элемента
-        disabledClass: 'dropdown__item__disabled' // класс disabled элемента
+        activeClass: 'dropdown__item-active', // класс активного элемента
+        disabledClass: 'dropdown__item-disabled' // класс disabled элемента
       }
     }
   }
 
+  // Выбираем все дропдауны на страницы
+  var dropdowns = $('.dropdown')
+
+  // Перебираем все результаты и объявляем view
+  dropdowns.each(function(key, value) {
+    var view = window.ru.mail.cpf.modules.Dropdown(dropdownParams, null, $(value));
+  });
 
 })(window);
